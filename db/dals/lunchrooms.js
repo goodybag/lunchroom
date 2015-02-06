23636e567ae5a72f5e40fa149a940d349996dee6
Preston Pham <prestonp08@gmail.com>
2015-02-06 16:48:10 -0600

    init other basic tables

--- db/dals/lunchrooms.js
+++ db/dals/lunchrooms.js
@@ -4,5 +4,14 @@ module.exports = {
 , schema: {
     id:         { type: 'int', primaryKey: true }
   , createdAt:  { type: 'timestamp', default: 'now()' }
+  , user_name: { type: 'text' }
+  , user_email: { type: 'text' }
+  , organization: { type: 'text' }
+  , street: { type: 'text' }
+  , street2: { type: 'text' }
+  , city: { type: 'text' }
+  , state: { type: 'varchar(10)' }
+  , zip: { type: 'varchar(5)', checks: ["SIMILAR TO '[[:digit:]]{5}'"] }
+  , time: { type: 'time without time zone' }
   }
 };
