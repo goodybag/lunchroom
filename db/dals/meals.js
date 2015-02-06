67fc97b730baf46c6cf683068fbce7cfdda87bdf
Preston Pham <prestonp08@gmail.com>
2015-02-06 17:45:37 -0600

    wip tables

--- db/dals/meals.js
+++ db/dals/meals.js
@@ -2,11 +2,11 @@ module.exports = {
   name: 'meals'
 
 , schema: {
-    id:         { type: 'int', primaryKey: true }
-  , createdAt:  { type: 'timestamp', default: 'now()' }
+    id: { type: 'int', primaryKey: true }
+  , createdAt: { type: 'timestamp', default: 'now()' }
   , user_name: { type: 'text' }
   , user_email: { type: 'text' }
-  , payment_method_id: { type: 'id', references: { table: 'payment_methods', column: 'id' } }
+  , payment_method_id: { type: 'int', references: { table: 'payment_methods', column: 'id' } }
   , sales_tax: { type: 'int', default: 0 }
   , total: { type: 'int', default: 0 }
   }
