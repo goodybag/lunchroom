b8187f8e55a93f4f49eedf9960d86aa8545bae61
John Fawcett <jrf0110@gmail.com>
2015-02-22 22:21:20 -0600

    WIP

--- server/routes/admin/index.js
+++ server/routes/admin/index.js
@@ -2,6 +2,9 @@ var m = require('../../middleware');
 var app = module.exports = require('express').Router();
 
 app.get('/kitchen-sink'
+, m.locals({
+    items: []
+  })
 , m.view('routes/admin/views/kitchen-sink', {
     layout: 'views/layout'
   })
