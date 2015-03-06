8f84cbd39bf8d2ef3f96ae569f8adae366ab9c15
John Fawcett <jrf0110@gmail.com>
2015-03-06 14:21:29 -0600

    add herounit, fix dollars stuff

--- server/routes/admin/index.js
+++ server/routes/admin/index.js
@@ -1,67 +1,60 @@
 var m = require('../../middleware');
 var app = module.exports = require('express').Router();
-var usd = require('usd');
 
 app.get('/kitchen-sink'
 , m.locals({
     items:  [ { name: 'Pirata Dos Tacos'
-              , price: usd().pennies(1090)
+              , price: 1090
               , dietTags: ['gluten-free', 'spicy']
               , coverPhotoUrl: 'https://cdn.filepicker.io/api/file/JEjlpwrGSrK4D9VjpHkc'
               }
             , { name: 'Poop Taco Box'
-              , price: usd().pennies(1200)
+              , price: 1200
               , dietTags: ['gluten-free', 'spicy']
               , coverPhotoUrl: 'https://cdn.filepicker.io/api/file/JEjlpwrGSrK4D9VjpHkc'
               }
             , { name: 'Pirata Dos Tacos'
-              , price: usd().pennies(1090)
+              , price: 1090
               , dietTags: ['gluten-free', 'spicy']
               , coverPhotoUrl: 'https://cdn.filepicker.io/api/file/JEjlpwrGSrK4D9VjpHkc'
               }
             , { name: 'Poop Taco Box'
-              , price: usd().pennies(1200)
+              , price: 1200
               , dietTags: ['gluten-free', 'spicy']
               , coverPhotoUrl: 'https://cdn.filepicker.io/api/file/JEjlpwrGSrK4D9VjpHkc'
               }
             , { name: 'Poop Taco Box'
-              , price: usd().pennies(1200)
+              , price: 1200
               , dietTags: ['gluten-free', 'spicy']
               , coverPhotoUrl: 'https://cdn.filepicker.io/api/file/JEjlpwrGSrK4D9VjpHkc'
               }
             , { name: 'Pirata Dos Tacos'
-              , price: usd().pennies(1090)
+              , price: 1090
               , dietTags: ['gluten-free', 'spicy']
               , coverPhotoUrl: 'https://cdn.filepicker.io/api/file/JEjlpwrGSrK4D9VjpHkc'
               }
             , { name: 'Poop Taco Box'
-              , price: usd().pennies(1200)
+              , price: 1200
               , dietTags: ['gluten-free', 'spicy']
               , coverPhotoUrl: 'https://cdn.filepicker.io/api/file/JEjlpwrGSrK4D9VjpHkc'
               }
             , { name: 'Poop Taco Box'
-              , price: usd().pennies(1200)
+              , price: 1200
               , dietTags: ['gluten-free', 'spicy']
               , coverPhotoUrl: 'https://cdn.filepicker.io/api/file/JEjlpwrGSrK4D9VjpHkc'
               }
             , { name: 'Pirata Dos Tacos'
-              , price: usd().pennies(1090)
+              , price: 1090
               , dietTags: ['gluten-free', 'spicy']
               , coverPhotoUrl: 'https://cdn.filepicker.io/api/file/JEjlpwrGSrK4D9VjpHkc'
               }
             , { name: 'Poop Taco Box'
-              , price: usd().pennies(1200)
+              , price: 1200
               , dietTags: ['gluten-free', 'spicy']
               , coverPhotoUrl: 'https://cdn.filepicker.io/api/file/JEjlpwrGSrK4D9VjpHkc'
               }
             ]
-  }, { cloneDeep: false })
-, function( req, res, next ){
-    res.locals.items.forEach(function(item){
-      console.log(item.price.toDollars());
-    });
-    next();
-  }
+  })
 , m.view('routes/admin/views/kitchen-sink', {
     layout: 'views/layout'
   })
