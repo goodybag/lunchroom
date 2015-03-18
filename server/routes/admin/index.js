28b96ec254420f63618ce88530695324190e0126
John Fawcett <jrf0110@gmail.com>
2015-03-18 15:48:17 -0500

    fixup buttons the item cards

--- server/routes/admin/index.js
+++ server/routes/admin/index.js
@@ -10,6 +10,21 @@ app.get('/kitchen-sink'
               }
             , { name: 'Poop Taco Box'
               , price: 1200
+              , dietTags: ['vegan', 'vegetarian']
+              , coverPhotoUrl: 'https://cdn.filepicker.io/api/file/JEjlpwrGSrK4D9VjpHkc'
+              }
+            , { name: 'Pirata Dos Tacos'
+              , price: 1090
+              , dietTags: []
+              , coverPhotoUrl: 'https://cdn.filepicker.io/api/file/JEjlpwrGSrK4D9VjpHkc'
+              }
+            , { name: 'Poop Taco Box'
+              , price: 1200
+              , dietTags: ['gluten-free', 'spicy']
+              , coverPhotoUrl: 'https://cdn.filepicker.io/api/file/JEjlpwrGSrK4D9VjpHkc'
+              }
+            , { name: 'Poop Taco Box'
+              , price: 1200
               , dietTags: ['gluten-free', 'spicy']
               , coverPhotoUrl: 'https://cdn.filepicker.io/api/file/JEjlpwrGSrK4D9VjpHkc'
               }
@@ -30,26 +45,6 @@ app.get('/kitchen-sink'
               }
             , { name: 'Pirata Dos Tacos'
               , price: 1090
-              , dietTags: ['gluten-free', 'spicy']
-              , coverPhotoUrl: 'https://cdn.filepicker.io/api/file/JEjlpwrGSrK4D9VjpHkc'
-              }
-            , { name: 'Poop Taco Box'
-              , price: 1200
-              , dietTags: ['gluten-free', 'spicy']
-              , coverPhotoUrl: 'https://cdn.filepicker.io/api/file/JEjlpwrGSrK4D9VjpHkc'
-              }
-            , { name: 'Poop Taco Box'
-              , price: 1200
-              , dietTags: ['gluten-free', 'spicy']
-              , coverPhotoUrl: 'https://cdn.filepicker.io/api/file/JEjlpwrGSrK4D9VjpHkc'
-              }
-            , { name: 'Pirata Dos Tacos'
-              , price: 1090
-              , dietTags: ['gluten-free', 'spicy']
-              , coverPhotoUrl: 'https://cdn.filepicker.io/api/file/JEjlpwrGSrK4D9VjpHkc'
-              }
-            , { name: 'Poop Taco Box'
-              , price: 1200
               , dietTags: ['gluten-free', 'spicy']
               , coverPhotoUrl: 'https://cdn.filepicker.io/api/file/JEjlpwrGSrK4D9VjpHkc'
               }
