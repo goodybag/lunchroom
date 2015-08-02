114ebdb8f0b29e8ca78a359fa430730dbcd71f83
Christoph Dorn <christoph@christophdorn.com>
2015-08-01 23:04:36 -0700

    wip: deploy

---
+++ server/jobs.js
@@ -0,0 +1,11 @@
+
+require('org.pinf.genesis.lib').forModule(require, module, function (API, exports) {
+
+    exports.notifyNewConsumerGroupEvent = function (event_id) {
+
+
+console.log("event_id", event_id);
+
+    }
+
+});
