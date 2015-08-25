4f91592f4f742feac118e6b3f9f769abdfb0a049
Christoph Dorn <christoph@christophdorn.com>
2015-08-25 09:01:18 -0700

    build

--- .components.built/bundle.js
+++ .components.built/bundle.js
@@ -508,11 +508,11 @@
 
 		var skin = getSkin();
 		var appContext = initAppContext(skin);
-
-		window.console = __webpack_require__(293)['for']({
+	/*
+		window.console = require("../stores/ui.Logging")['for']({
 			appContext: appContext
 		}).getConsole();
-
+	*/
 		appContext.set('initialized', true);
 
 		$(function () {
@@ -77023,82 +77023,7 @@
 
 
 /***/ },
-/* 293 */
-/***/ function(module, exports, __webpack_require__) {
-
-	/** @jsx React.DOM */
-	exports['for'] = function (context) {
-
-		var dev = context.appContext.get("context").dev;
-		var services = context.appContext.get("context").services;
-
-		var browserConsole = window.console;
-
-		var Q = __webpack_require__(11);
-
-		var exports = {};
-
-		function getLogger () {
-			if (!getLogger._logger) {
-				getLogger._logger = Q.denodeify(function (callback) {
-					var checkInterval = setInterval(function () {
-						if (!window._LTracker) return;
-
-						clearInterval(checkInterval);
-						if (
-							services &&
-							services.loggly
-						) {
-							window._LTracker.push(services.loggly);
-							return callback(null, window._LTracker);
-						}
-
-						return callback(null, null);
-					}, 100);
-				})();
-			}
-			return getLogger._logger;
-		}
-
-		exports.getConsole = function () {
-
-			var console = {};
-			[
-				"log",
-				"info",
-				"warn",
-				"error",
-				"trace"
-			].forEach(function (type) {
-				console[type] = function () {
-					var args = Array.prototype.slice.call(arguments);
-					getLogger().then(function (logger) {
-
-						browserConsole[type].apply(browserConsole, args);
-
-						if (
-							logger &&
-							(
-								type === "error" ||
-								type === "log"
-							)
-						) {
-							logger.push({
-								"app": "lunchroom-client",
-								"args": args
-							});
-						}
-					});
-				};
-			});
-			return console;
-		}
-
-		return exports;
-	}
-
-
-/***/ },
+/* 293 */,
 /* 294 */
 /***/ function(module, exports, __webpack_require__, __webpack_module_template_argument_0__) {
 
